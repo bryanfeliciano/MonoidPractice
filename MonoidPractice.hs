c2474b0ed2ba67a838c3f9a723546a22cb7e56b3
bryan <bryanfeliciano892@gmail.com>
2022-04-23 11:03:08 -0500

    cartesian product added

--- MonoidPractice.hs
+++ MonoidPractice.hs
@@ -1,9 +1,9 @@
 import Data.Semigroup
 
-class Monoid a where
-    mempty :: [a] -> a
-    mappend :: a -> a -> a
-    mconcat :: [a] -> a
+-- class Monoid a where
+--     mempty :: [a] -> a
+--     mappend :: a -> a -> a
+--     mconcat :: [a] -> a
 
 -- Practical Monoids - Probability tables --
 
@@ -17,3 +17,21 @@ createPTable events probs = PTable events normalizedProbs
   where
       totalProbs = sum probs
       normalizedProbs = map (\x -> x/totalProbs) probs
+
+showPair :: String -> Double -> String
+showPair event prob = mconcat [event, "|" , show prob , "\n"]
+
+instance Show PTable where
+    show (PTable events probs) = mconcat pairs
+      where 
+          pairs = zipWith showPair events probs
+
+-- Apply the cartesian product  --
+
+cartCombine :: (a -> b -> c) -> [a] -> [b] -> [c]
+cartCombine func l1 l2 = zipWith func newL1 cycledL2
+  where
+      nToAdd = length l2
+      repeatedL1 = map (take nToAdd . repeat) l1
+      newL1 = mconcat repeatedL1
+      cycledL2 = cycle l2 
